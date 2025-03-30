7f2b99f79a79547e843305876c3b4b4606618f83
PrzemyslawZych <252843@edu.p.lodz.pl>
2025-03-30 20:04:25 +0200

    6.1: Zastąpienie literałów deklaracjami stałych - poprawki

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,13 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int CAPACITY = 12;
-    public static final int CALLCHECK = -1;
-    public static final int FULLINDEX = 11;
-    public static final int ERROR = -1;
-    final private int[] numbers = new int[CAPACITY];
+    private static final int MAX_STACK_CAPACITY = 12;
+    public static final int EMPTY_RHYMER_INDICATOR = -1;
+    public static final int MAX_STACK_INDEX = MAX_STACK_CAPACITY - 1;
+    public static final int DEFAULT_RETURN_VALUE = -1;
+    final private int[] numbers = new int[MAX_STACK_CAPACITY];
 
-    public int total = -1;
+    public int total = EMPTY_RHYMER_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -16,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == CALLCHECK;
+        return total == EMPTY_RHYMER_INDICATOR;
     }
 
     public boolean isFull() {
-        return total == FULLINDEX;
+        return total == MAX_STACK_INDEX;
     }
 
     protected int peekAtTop() {
         if (callCheck())
-            return ERROR;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return ERROR;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total--];
     }
 
